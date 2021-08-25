c121dfd44cd1800ec1943b94a97ef78e8f45cd36
Akwasi Atakora-Acheampong <aa.acheampong1@gmail.com>
2021-08-25 17:12:02 +0100

    MVP complete, some testing required

---
+++ src/main/java/Staff/Management/Director.java
@@ -0,0 +1,17 @@
+package Staff.Management;
+
+
+
+public class Director extends Manager {
+
+    private double budget;
+
+    public Director(String deptName, String name, String niNumber, double salary, double budget){
+        super(deptName, name, niNumber, salary);
+        this.budget = budget;
+    }
+
+    public double getBudget() {
+        return budget;
+    }
+}
